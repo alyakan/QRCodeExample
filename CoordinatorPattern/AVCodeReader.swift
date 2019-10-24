8f4d1e9326e5848516074a4b0c0bbb170999ec54
Aly Yakan <a.yakan@newmotion.com>
2019-10-24 11:07:35 +0200

    Scan QR code from a certain point of interest

--- CoordinatorPattern/AVCodeReader.swift
+++ CoordinatorPattern/AVCodeReader.swift
@@ -7,20 +7,35 @@
 //
 
 import AVFoundation
+import UIKit
 
 protocol CodeReader {
     var videoPreview: CALayer { get }
+    var rectOfInterest: CGRect { get set }
     func startReading(completion: @escaping (String) -> Void)
     func stopReading()
 }
 
 class AVCodeReader: NSObject {
+    var rectOfInterest: CGRect {
+        didSet {
+            guard let captureVideoPreview = captureVideoPreview else {
+                return
+            }
+
+            captureMetadataOutput.rectOfInterest = captureVideoPreview.metadataOutputRectConverted(fromLayerRect: rectOfInterest)
+        }
+    }
+
     fileprivate(set) var videoPreview = CALayer()
+    fileprivate var captureMetadataOutput = AVCaptureMetadataOutput()
+    fileprivate var captureVideoPreview: AVCaptureVideoPreviewLayer?
 
     fileprivate var captureSession: AVCaptureSession?
     fileprivate var didRead: ((String) -> Void)?
 
     override init() {
+        rectOfInterest = CGRect.zero
         super.init()
 
         //Make sure the device can handle video
@@ -36,26 +51,30 @@ class AVCodeReader: NSObject {
         captureSession?.addInput(deviceInput)
 
         //output
-        let captureMetadataOutput = AVCaptureMetadataOutput()
         captureSession?.addOutput(captureMetadataOutput)
         captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+
         //interprets qr codes only
         captureMetadataOutput.metadataObjectTypes = [.qr]
 
         //preview
         guard let captureSession = captureSession else { return }
-        let captureVideoPreview = AVCaptureVideoPreviewLayer(session: captureSession)
+        captureVideoPreview = AVCaptureVideoPreviewLayer(session: captureSession)
+        guard let captureVideoPreview = captureVideoPreview else { return }
         captureVideoPreview.videoGravity = .resizeAspectFill
         self.videoPreview = captureVideoPreview
     }
+
+
 }
 
 extension AVCodeReader: AVCaptureMetadataOutputObjectsDelegate {
     func metadataOutput(_ captureOutput: AVCaptureMetadataOutput,
                         didOutput metadataObjects: [AVMetadataObject],
                         from connection: AVCaptureConnection) {
-        guard let readableCode = metadataObjects.first as? AVMetadataMachineReadableCodeObject,
-              let code = readableCode.stringValue else {
+        guard
+            let readableCode = metadataObjects.first as? AVMetadataMachineReadableCodeObject,
+            let code = readableCode.stringValue else {
              return
         }
 
@@ -72,7 +91,7 @@ extension AVCodeReader: CodeReader {
         self.didRead = completion
         captureSession?.startRunning()
     }
-    
+
     func stopReading() {
         captureSession?.stopRunning()
     }
--- CoordinatorPattern/ReaderViewController.swift
+++ CoordinatorPattern/ReaderViewController.swift
@@ -19,11 +19,13 @@ class ReaderViewController: UIViewController, Storyboarded {
     override func viewDidLoad() {
         videoLayer = codeReader.videoPreview
         videoPreview.layer.addSublayer(videoLayer)
+        addAreaOfInterest()
     }
 
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         videoLayer.frame = videoPreview.bounds
+        codeReader.rectOfInterest = CGRect(x: 100, y: 100, width: 100, height: 100)
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -41,10 +43,21 @@ class ReaderViewController: UIViewController, Storyboarded {
     }
 
     private func startReading() {
-        codeReader.startReading { [weak self] (code) in
+        codeReader.startReading { [weak self] code in
+            guard let self = self else { return }
             DispatchQueue.main.async {
-                self?.coordinator?.showDetails(details: code)
+                self.coordinator?.showDetails(details: code)
             }
         }
+    }
+
+    private func addAreaOfInterest() {
+        let view = UIView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+        view.backgroundColor = .clear
+        view.layer.borderWidth = 1
+        view.layer.borderColor = UIColor.systemBlue.cgColor
+        view.layer.cornerRadius = 2
+
+        videoPreview.addSubview(view)
     }
 }
